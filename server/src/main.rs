5989738a4f227ab34b7447daa1319bcbee842723
Cory Sabol <corygrandhamsabol@gmail.com>
2024-01-26 21:24:51 -0500

    rust version init commit

---
+++ server/src/main.rs
@@ -0,0 +1,36 @@
+use actix::{Actor, StreamHandler};
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web_actors::ws;
+
+/// Define HTTP actor
+struct WSActor;
+
+impl Actor for WSActor {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+/// Handler for ws::Message message
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WSActor {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(ws::Message::Text(text)) => ctx.text(format!("Here is your input: {}", text)),
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            _ => (),
+        }
+    }
+}
+
+async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let resp = ws::start(WSActor {}, &req, stream);
+    println!("{:?}", resp);
+    resp
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/chat", web::get().to(index)))
+        .bind(("127.0.0.1", 8089))?
+        .run()
+        .await
+}
