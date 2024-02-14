dbadb68acbf73692280c3252f4033382ae6d7965
Cory Sabol <corygrandhamsabol@gmail.com>
2024-02-14 01:39:50 -0500

    hacking together text inference via websocket server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,22 @@
 use actix::{Actor, StreamHandler};
 use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
+use anyhow::Error as E;
 
 // Inference stuff
-use inference::*;
+use inference::{text_generation::TextGeneration, *};
 
 /// Define HTTP actor
-struct WSActor;
+struct WSActor {
+    text_pipeline: TextGeneration,
+}
+
+impl WSActor {
+    fn text_generation(&mut self, prompt: &str, sample_len: usize) -> Result<(), E> {
+        // Need to update the text generation code to return the generated text
+        self.text_pipeline.run(prompt, sample_len)
+    }
+}
 
 impl Actor for WSActor {
     type Context = ws::WebsocketContext<Self>;
@@ -15,9 +25,20 @@ impl Actor for WSActor {
 /// Handler for ws::Message message
 impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WSActor {
     fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        // Initialize the text generation pipeline
         match msg {
             Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(text)) => ctx.text(format!("Here is your input: {}", text)),
+            Ok(ws::Message::Text(text)) => {
+                // Extract the prompt from the request
+                let prompt: &str = std::str::from_utf8(text.as_bytes()).unwrap();
+                // TODO: Extract sample_len from reqeust?
+                let r = self.text_generation(prompt, 256); // Hardcode 256 sample_len for the time being
+                match r {
+                    Ok(_) => (),
+                    Err(e) => (), // TODO implement error handling
+                }
+                ctx.text(format!("Here is your input: {}", text))
+            }
             Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
             _ => (),
         }
@@ -25,7 +46,11 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WSActor {
 }
 
 async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    let resp = ws::start(WSActor {}, &req, stream);
+    let actor = WSActor {
+        text_pipeline: TextGeneration::new(None, None, 299792458, None, None, 1.1, 64, false)
+            .unwrap(),
+    };
+    let resp = ws::start(actor, &req, stream);
     println!("{:?}", resp);
     resp
 }
@@ -37,3 +62,4 @@ async fn main() -> std::io::Result<()> {
         .run()
         .await
 }
+
